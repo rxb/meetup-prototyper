15d21f3f2e75a0a53abff7c25e06cddbdbebf2bc
rxb <boenigk@gmail.com>
2014-09-15 19:58:33 -0400

    spring cleaning

---
+++ js/demo_groups.js
@@ -0,0 +1,51 @@
+var DEMO_GROUPS = [
+	{
+		extraContext: {
+			banner: "http://photos4.meetupstatic.com/photos/event/e/1/f/c/highres_323457852.jpeg"
+		},
+		shoppingList: [
+				{"gimme": "groups", "page": 5},		
+				{"gimme": "group", "group_id": 1227102},
+				{"gimme": "events", "page": 15, "group_id": 1227102},
+				{"gimme": "members", "page": 40, "group_id": 1227102},
+				{"gimme": "discussions", "page": 9, "urlkey": "shutterbugexcursions", "board_id": 854675}
+			]
+	},
+	{
+		extraContext: {
+			banner: "http://photos2.meetupstatic.com/photos/event/6/c/f/4/highres_282927892.jpeg"
+		},
+		shoppingList: [
+				{"gimme": "groups", "page": 5},		
+				{"gimme": "group", "group_id": 1052584},
+				{"gimme": "events", "page": 15, "group_id": 1052584},
+				{"gimme": "members", "page": 40, "group_id": 1052584},
+				{"gimme": "discussions", "page": 9, "urlkey": "adventurers-237", "board_id": 884899}
+			]
+	},
+	{
+		extraContext: {
+			banner: "http://photos1.meetupstatic.com/photos/event/d/e/0/0/highres_317996832.jpeg"
+		},
+		shoppingList: [
+				{"gimme": "groups", "page": 5},		
+				{"gimme": "group", "group_id": 5163882},
+				{"gimme": "events", "page": 15, "group_id": 5163882},
+				{"gimme": "members", "page": 40, "group_id": 5163882},
+				{"gimme": "discussions", "page": 9, "urlkey": "7-Languages-in-7-Months-NYC", "board_id": 6413112}
+		]
+	},
+	{
+		extraContext: {
+			banner: "http://photos4.meetupstatic.com/photos/event/c/c/4/4/highres_324112292.jpeg"
+		},
+		shoppingList: [
+				{"gimme": "groups", "page": 5},		
+				{"gimme": "group", "group_id": 1618009},
+				{"gimme": "events", "page": 15, "group_id": 1618009},
+				{"gimme": "members", "page": 40, "group_id": 1618009},
+				{"gimme": "discussions", "page": 9, "urlkey": "Stroller-Strides-of-Midtown-Raleigh", "board_id": 1278016}
+		]
+	}
+
+];
